e0ef7cbc12b427f28945d6b20e1b8028c2881a40
khushi <khushisidmutha@gmail.com>
2024-02-01 09:03:08 +0530

    file added

---
+++ div.java
@@ -0,0 +1,8 @@
+ic class div{
+    public static void main(String[] args){
+        int a=5;
+        int b=6;
+        System.out.println(a*b);
+        System.out.println("Khushi mutha")
+    }
+}
